c9a9e493deead102d58a54e9118f09a727113601
Jeison Fuentes Guerra <jeisondfuentes@unicesar.edu.co>
2026-05-07 19:03:03 -0500

    feat: add GetAll method to AcademicProgram, IdentificationType, and Role controllers with AllowAnonymous attribute

--- Api/Controllers/IdentificationTypeController.cs
+++ Api/Controllers/IdentificationTypeController.cs
@@ -1,6 +1,9 @@
+using Application.Shared.DTOs;
 using Application.Shared.DTOs.IdentificationTypes;
 using Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
@@ -9,5 +12,13 @@ namespace Api.Controllers
         public IdentificationTypeController(IMediator mediator) : base(mediator)
         {
         }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetById(int id) => await base.GetById(id);
+
+        [AllowAnonymous]
+        [HttpGet]
+        public override async Task<IActionResult> GetAll([FromQuery] PaginatedRequest request) => await base.GetAll(request);
     }
 }
